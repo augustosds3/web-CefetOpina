b76ef015d4f24561636fa1c6f55f269324ac8ea5
augustosds3 <augustosd3@gmail.com>
2017-04-28 22:17:17 -0300

    Versão final (a ser ajustada em etapas posteriores)

---
+++ JS/selecao.js
@@ -0,0 +1,10 @@
+var modal = document.getElementById('modalSelecao');
+var modalConten = document.getElementById('modalContent')
+var btn = document.getElementById('cadastroButton');
+
+
+btn.onclick = function(){
+	debugger;
+	modal.style.display = "block";
+	modalContent.classList.add("modal-content");
+}
